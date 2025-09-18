a6d0a01757b73e8fe83e57c73083ccb08a49e20f
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-09-18 21:34:34 +0300

    cache configurations

--- MUST/settings.py
+++ MUST/settings.py
@@ -207,3 +207,16 @@ LOGOUT_REDIRECT_URL = "/"
 JWT_SECRET = os.environ.get('JWT_SECRET')
 JWT_ALGORITHM = "HS256"
 
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'KEY_PREFIX':'events_api',
+        'TIMEOUT': 300,
+    }
+}
+
